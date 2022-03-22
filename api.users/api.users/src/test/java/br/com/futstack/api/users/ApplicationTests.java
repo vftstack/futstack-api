b3b498fa7dea83c062822cc43441fca66741f94a
vftstack <victorrds18@outlook.com>
2022-03-22 15:13:54 -0300

    criação-do-projeto

---
+++ api.users/api.users/src/test/java/br/com/futstack/api/users/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futstack.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
